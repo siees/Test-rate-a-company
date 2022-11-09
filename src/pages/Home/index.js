b9550e7bde8db716d8e915526a5b36583b620a01
Nouri Hrairi <medanouar.hrairi@gmail.com>
2022-11-09 05:11:35 +0100

    Home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import Banner from './Banner';
+import Types from './Types';
+import TestimonialList from './Testimonial-list';
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <Types />
+      <TestimonialList />
+    </div>
+  );
+};
+
+export default Home;
